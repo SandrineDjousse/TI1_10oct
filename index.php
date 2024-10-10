b202abf9f6e4644f6af78679033ee1d703c22416
Sandrine Djousse Tsoyem <sandytsoyem@gmail.com>
2024-10-10 10:24:33 +0200

    Début des exercices 1&2

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!doctype html>
+<html>
+<head>
+    <title>TI 1</title>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./lib/css/style.css">
+    <script src="./lib/js/fonctions1.js" defer></script>
+</head>
+<body>
+<header class="container image_header">
+
+</header>
+
+<footer class="container">
+    Footer
+</footer>
+</body>
+</html>
---
+++ pages/ex01.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+    <title>TI 1</title>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../lib/css/style.css">
+    <script src="../lib/js/exo1.js" defer></script>
+</head>
+<body>
+<header >
+
+</header>
+<h3>petit_1_a</h3>
+Valeur initiale : <div id="annee1">
+
+</div>
+Valeur finale : <div id="annee2">
+
+</div>
+    <hr>
+
+<h3> petit_1_b</h3>
+<div>
+        Longueur  : <span id="longueur">25</span><br>
+        Largeur   : <span id="largeur">10</span><br>
+        Périmètre : <span id="perimetre"></span><br>
+        Aire      : <span id="aire"></span><br>
+
+</div>
+<hr>
+
+<footer>
+
+</footer>
+</body>
+</html>
